201c0a6d277b1c33263049daf52f9609083a2d91
Fomin <newfomin@gmail.com>
2020-03-26 23:19:42 +0400

    33

--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -9,6 +9,8 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
 
+
+
     let addPost = () => {
         let text = newPostElement.current.value;
         props.addPost(text);
