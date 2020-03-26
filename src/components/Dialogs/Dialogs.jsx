397ae09b8f0716f44dc3d3c130234b8f3dc206b2
elik <elik@example.com>
2020-03-26 20:32:14 +0400

    31 dom

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,14 +5,15 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 
-
-
 const Dialogs = (props) => {
-    let dialogElements = props.state.dialogs.map( d=> <DialogItem name={d.name} id={d.id}/>)
-    let messagesElement = props.state.messages.map( m=> <Message id={m.id} message={m.massage}/>)
+    let dialogElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElement = props.state.messages.map(m => <Message id={m.id} message={m.massage}/>)
 
-
-
+let newMessage = React.createRef();
+    let addnewMessage = () => {
+        let text = newMessage.current.value;
+        alert(text)
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
@@ -20,7 +21,9 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElement}
-                </div>
+                <textarea ref={newMessage}></textarea>
+                <button onClick={addnewMessage}>Add Message</button>
+            </div>
 
         </div>
 
