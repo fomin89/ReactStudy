4e4d8f1f755adc6a0c07add178163469ddc7c9fb
Fomin <newfomin@gmail.com>
2020-03-27 09:38:50 +0400

    34

--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -4,26 +4,28 @@ import Post from "./post/Post";
 
 
 const MyPosts = (props) => {
-    let postsElements=props.posts.map(p=> <Post id={p.id} message={p.message} LikesCount={p.likesCount}/>)
+    let postsElements = props.posts.map(p => <Post id={p.id} message={p.message} LikesCount={p.likesCount}/>)
 
     let newPostElement = React.createRef();
 
 
-
-
     let addPost = () => {
-        let text = newPostElement.current.value;
-        props.addPost(text);
-        newPostElement.current.value =''
+        props.addPost()
     }
 
-
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        props.updateNewPostText(text)
+    }
 
     return <div className={s.postsBlock}>
         <h3>My posts</h3>
         <div>
             <div>
-                <textarea ref={newPostElement}></textarea>
+                <textarea onChange={onPostChange}
+                          value={props.newPostText}
+                          ref={newPostElement}
+                />
             </div>
             <div>
                 <button onClick={addPost}> Add post</button>
--- src/redux/state.js
+++ src/redux/state.js
@@ -6,7 +6,7 @@ let state = {
             {id: 1, message: 'Hi, how are you?', likesCount: 0},
             {id: 2, message: 'Its my first post', likesCount: 12},
         ],
-
+newPostText: 'fgrgreergergr'
 
     },
     dialogsPage: {
@@ -26,17 +26,22 @@ let state = {
 
         ]
     }
-
-
 }
-export let addPost = (postMessage) =>{
+
+
+export let addPost = () =>{
     let newPost = {
         id: 3,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCount: 21
 
     }
     state.profilePage.posts.push(newPost)
+    state.profilePage.newPostText=''
+    rerenderEntireTree(state)
+}
+export let updateNewPostText =(newText) => {
+    state.profilePage.newPostText = newText;
     rerenderEntireTree(state)
 }
 
